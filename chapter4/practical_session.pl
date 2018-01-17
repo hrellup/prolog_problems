e8437a6451740466df2ed0dc96e4a029485245fc
heinz rellup <heinz.rellup@freenet.de>
2018-01-17 08:48:16 +0100

    Signed-off-by: heinz rellup <heinz.rellup@freenet.de>

--- chapter4/practical_session.pl
+++ chapter4/practical_session.pl
@@ -28,5 +28,28 @@ combine1([AH|AT],[BH|BT],[AH,BH|C]) :- combine1(AT,BT,C).
 % Result = [[f,glu],[b,gla],[yip,gli],[yup,glo]]
 
 combine2([],[],[]).
-combine2([AH|AT],[BH|BT],[CH,CT]) :- combine2(AT,BT,CT).
+combine2([AH|AT],[BH|BT], [[AH,BH]|C]) :- combine2(AT,BT,C).
+
+% Finally, write a 3-place predicate combine3 which takes three lists as
+% arguments and combines the elements of the first two lists into the
+% third as follows:
+%
+% ?- combine3([a,b,c],[1,2,3],X).
+%
+% X = [j(a,1),j(b,2),j(c,3)]
+%
+% ?- combine3([f,b,yip,yup],[glu,gla,gli,glo],R).
+%
+% R = [j(f,glu),j(b,gla),j(yip,gli),j(yup,glo)]
+
+combine3([],[],[]).
+combine3([AH|AT],[BH|BT], [j(AH,BH)|C]) :- combine3(AT,BT,C).
+
+
+
+
+
+
+
+
 
